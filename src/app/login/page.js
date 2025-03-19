52ba9ed00152d1b2840439a2f4fcb948bf6f1c93
Tiiickk <fabian.cange@hotmail.fr>
2025-03-19 12:29:51 +0100

    Added login style // Finally Done

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -23,15 +23,32 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="auth-container">
-      <h1>Connexion</h1>
-      {error && <p className="error-message">{error}</p>}
-      <form onSubmit={handleLogin}>
-        <input type="text" placeholder="Nom d'utilisateur" value={username} onChange={(e) => setUsername(e.target.value)} required />
-        <input type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        <button type="submit">Se connecter</button>
-      </form>
-      <p>Pas encore de compte ? <a href="/register">S&apos;inscrire</a></p>
+    <div className="auth-page">
+      <img src="/img/heroBanner/books-1842306_1920.jpg" className="authImg" />
+      <div className="auth-container">
+        <h1>Connexion</h1>
+        {error && <p className="error-message">{error}</p>}
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Nom d'utilisateur"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Se connecter</button>
+        </form>
+        <p>
+          Pas encore de compte ? <a href="/register">S&apos;inscrire</a>
+        </p>
+      </div>
     </div>
   )
 }
